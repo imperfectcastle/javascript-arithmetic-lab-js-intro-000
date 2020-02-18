2edbd6ef1b2fa5783c2383f71776ad0fbecd9d75
Bart Sakowski <bart.sakowski@gmail.com>
2020-02-18 01:38:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function decrement(number) {
   return number
 }
 
-function makeInt(n) {
-  parseInt('n', 10)
+function makeInt(n, 10) {
+  parseInt(n, 10)
 }
 
 function preserveDecimal(n) {
-  parseFloat('n')
+  parseFloat(n)
 }
